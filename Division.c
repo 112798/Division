21735cc77b35f25fd930e8e54951af89aeb6d3b9
112798 <71934627+112798@users.noreply.github.com>
2020-10-07 17:16:22 -0500

    Agregando programas

---
+++ Division.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	int a = 6;
+	int b = 2;
+	int c = 4;
+	
+	float d = 4.0;
+	
+	int res_01 =6/0;
+}
---
+++ Division_02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+	printf("Entero 6/2: %d\n", 6/2);
+	printf("Entero 6/4: %d\n", 6/4);
+	printf("Flotante 6.0/4.0: %.3f\n", (float)6/4);
+	}
